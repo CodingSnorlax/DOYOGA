b0eb94bcd1f7eabec5b2543c3cf402e9467f35e8
Karen <thinknfix2021@gmail.com>
2021-08-27 15:46:39 +0800

    for js 提問

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,24 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+
+
+// Initialize Swiper
+
+      // var swiper = new Swiper(".mySwiper", {
+      //   slidesPerView: 1,
+      //   centeredSlides: true,
+      //   spaceBetween: 30,
+        
+      // });
+
+      var swiper = new Swiper(".courseSwiper", {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerColumn: 2,
+        slidesPerColumnFill: "row",
+
+        breakpoints: {
+        992:{
+          slidesPerView: 1.5,
+          slidesPerColumn: 1,
+        },
+      }
+      });
