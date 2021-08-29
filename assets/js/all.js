6e0b24bde48aa1c972f1121549f25aafc5c83717
Karen <thinknfix2021@gmail.com>
2021-08-29 21:04:13 +0800

    Update 2021-08-29T13:04:11.474Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -6,6 +6,7 @@
 //   centeredSlides: true,
 //   spaceBetween: 30,
 // });
+// 課程的swiper
 var swiper = new Swiper(".courseSwiper", {
   slidesPerView: 2,
   spaceBetween: 30,
@@ -13,9 +14,60 @@ var swiper = new Swiper(".courseSwiper", {
   slidesPerColumnFill: "row",
   breakpoints: {
     992: {
-      slidesPerView: 1.5,
+      slidesPerView: 1.4,
       slidesPerColumn: 1
     }
   }
+}); // 聽聽他們怎麼說 swiper
+
+var swiper = new Swiper(".feedbackSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerColumn: 3,
+  slidesPerColumnFill: "row",
+  breakpoints: {
+    768: {
+      slidesPerView: 2.2,
+      slidesPerColumn: 2
+    },
+    992: {
+      slidesPerView: 3,
+      slidesPerColumn: 2
+    }
+  },
+  navigation: {
+    nextEl: ".swiper-btn-next",
+    prevEl: ".swiper-btn-prev"
+  }
+}); // 課程推薦 swiper 
+
+var swiper = new Swiper(".recommendSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2
+    },
+    992: {
+      slidesPerView: 3
+    },
+    1024: {
+      slidesPerView: 4
+    }
+  }
+}); // datepicker
+
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
+  nextArrow: '>',
+  prevArrow: '<',
+  buttonClass: 'btn datepicker-primary',
+  autohide: true,
+  clearBtn: true // format: 'mm/dd/yyyy',
+
 });
 //# sourceMappingURL=all.js.map
